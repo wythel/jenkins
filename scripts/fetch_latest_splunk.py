02aa7cee2a82f8f5a6b8911317db9436fd7c08f3
Wythe Lin <clin@1200477wythe.local>
2013-09-12 14:19:28 +0800

    Add options for splunk_dir and pkg_dir

--- scripts/fetch_latest_splunk.py
+++ scripts/fetch_latest_splunk.py
@@ -12,12 +12,12 @@ def print_time_spent(time, pkg):
     speed = (os.path.getsize(pkg) / 1024) / time
     print "Average speed: {s} kilobytes per second".format(s=speed)
 
-def dluntar(url):
+def dluntar(url, tar_dir, run_dir):
     """
     Dowload and then untar splunk package, calculate time spent also
     """
-    TAR_DIR = "/home/eserv/branches_tar"
-    RUN_DIR = "/home/eserv/splunk_run"
+    TAR_DIR = tar_dir #"/Users/clin/branches_tar"
+    RUN_DIR = run_dir #"/Users/clin/splunk_run"
     # download the tgz file
     # find the brach name and build number
 
@@ -45,34 +45,70 @@ def dluntar(url):
             exit(1)
 
     # untar the file
+    # get path
     branch_path = os.path.join(RUN_DIR, branch)
     if not os.path.exists(branch_path):
         os.mkdir(branch_path)
     untar_path = os.path.join(RUN_DIR, branch, build)
     if not os.path.exists(untar_path):
         os.mkdir(untar_path)
-    command = "tar xf {tar} -C {path}".format(tar=tar_file, path=untar_path)
-    print "Running '{c}'".format(c=command)
-    p = subprocess.Popen(command, env=os.environ, shell=True,
-                         stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    p.wait()
-    print os.path.join(untar_path, "splunk", "bin")
 
-def main():
-    # parse options
+    # if splunk is already there, just exit
+    if os.path.exists(os.path.join(untar_path, "splunk")):
+        print "Splunk is already in {p}, exiting".format(
+                                          p=os.path.join(untar_path, "splunk"))
+        exit(0)
+    else:
+        command = "tar xf {tar} -C {path}".format(tar=tar_file, path=untar_path)
+        print "Running '{c}'".format(c=command)
+        p = subprocess.Popen(command, env=os.environ, shell=True,
+                             stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        p.wait()
+        print os.path.join(untar_path, "splunk", "bin")
+
+def parse_options():
+    """
+    parse options
+    """
     parser = OptionParser()
     parser.add_option("-b", "--branch", dest="branch", help="brach to fetch")
     parser.add_option("-c", "--p4change", dest="cl", help="change list number to fetch")
+    parser.add_option("-p", "--pkg-dir", dest="pkg_dir",
+                      default="/Users/clin/branches_tar",
+                      help="directory for saving the pkg, will mkdir if it does not exist")
+    parser.add_option("-s", "--splunk-dir", dest="splunk_dir",
+                      default="/Users/clin/splunk_run",
+                      help="directory for untaring splunk, will mkdir if it does not exist")
     (options, args) = parser.parse_args()
+    return options
 
+def main():
+    options = parse_options()
     branch = options.branch
     cl = options.cl
+    pkg_dir = options.pkg_dir
+    splunk_dir = options.splunk_dir
 
+    # check if pkg_dir and splunk_dir exist. if not, create them
+    if not os.path.exists(pkg_dir):
+        os.makedirs(pkg_dir)
+
+    if not os.path.exists(splunk_dir):
+        os.makedirs(splunk_dir)
+
+    if branch is None:
+        print "Branch must be given, please use '-b' option to specify a branch"
+        exit(1)
+
+    # getting the pkg url
     url_template = ("http://releases.splunk.com/cgi-bin/splunk_build_fetcher.py"
                     "?PLAT_PKG=Darwin-universal.tgz&DELIVER_AS=url&BRANCH={b}")
     if cl is not None:
+        print "Getting splunk on branch '{b}' @ {cl}".format(b=branch, cl=cl)
         url_template = url_template + "&P4CHANGE={c}".format(c=cl)
+    else:
+        print "Getting the latest splunk pkg on branch '{b}'".format(b=branch)
 
     # get url of latest package
     f = urllib.urlopen(url_template.format(b=branch))
@@ -83,7 +119,7 @@ def main():
         exit(1)
     else:
         # run dluntar
-        dluntar(pkg_url)
+        dluntar(url=pkg_url, tar_dir=pkg_dir, run_dir=splunk_dir)
 
 if __name__ == '__main__':
     main()
