49df0eccd313b103066cbcab7233586d1ff4ac12
clin <clin@splunk.com>
2013-09-13 16:52:36 +0800

    echo when migration starts

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,8 +1,9 @@
+echo "Performing migration..."
 # stop splunk
 $WORKSPACE/splunk/bin/splunk stop
 
 #migrate
-python $WORKSPACE/scripts/fetch_splunk.py --branch=$OLD_BRANCH --pkg-dir=$WORKSPACE --splunk-dir=$WORKSPACE
+python $WORKSPACE/scripts/fetch_splunk.py --branch=$NEW_BRANCH --pkg-dir=$WORKSPACE --splunk-dir=$WORKSPACE
 
 # start splunk
 $WORKSPACE/splunk/bin/splunk start --accept-license --answer-yes
