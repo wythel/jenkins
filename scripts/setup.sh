d7e1fc10863065cd05dc169b6b705ffa49f9e746
clin <clin@splunk.com>
2013-09-18 15:17:34 +0800

    ADD script to set test enviroment

--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# get package
-python $WORKSPACE/scripts/fetch_splunk.py --branch=$OLD_BRANCH --pkg-dir=$WORKSPACE --splunk-dir=$WORKSPACE
-
-# start splunk
-$WORKSPACE/splunk/bin/splunk start --accept-license --answer-yes
