a798db39cb6894f65c0d6da9106c37d3c154c166
clin <clin@splunk.com>
2013-09-25 13:37:34 +0800

    add logger

--- scripts/fetch_splunk.py
+++ scripts/fetch_splunk.py
@@ -3,14 +3,15 @@ from optparse import OptionParser
 import os
 import subprocess
 from datetime import datetime
+import logging
 
 def print_time_spent(time, pkg):
     """
     print how much time spent and the avg internet speed
     """
-    print "Finished downloading, it took {t} seconds".format(t=time)
+    logger.info("Finished downloading, it took {t} seconds".format(t=time))
     speed = (os.path.getsize(pkg) / 1024) / time
-    print "Average speed: {s} kilobytes per second".format(s=speed)
+    logger.info("Average speed: {s} kilobytes per second".format(s=speed))
 
 def dluntar(url, tar_dir, run_dir):
     """
@@ -28,21 +29,21 @@ def dluntar(url, tar_dir, run_dir):
     tar_file = os.path.join(TAR_DIR, file_name)
     if not os.path.exists(tar_file):
         try:
-            print "Dowloading: {u}".format(u=url)
-            print "to: {f}".format(f=tar_file)
+            logger.info("Dowloading: {u}".format(u=url))
+            logger.info("to: {f}".format(f=tar_file))
             start = datetime.now()
             urllib.urlretrieve(url, tar_file)
             end = datetime.now()
             diff = end - start
             print_time_spent(time=diff.seconds, pkg=tar_file)
         except IOError, err:
-            print "Can not open url. Did you connect to Splunk VPN?"
-            print "ERROR: " + err
+            logger.error("Can not open url. Did you connect to Splunk VPN?")
+            logger,error("ERROR: " + err)
             exit(1)
 
     # untar the file
     command = "tar xf {tar} -C {path}".format(tar=tar_file, path=RUN_DIR)
-    print "Running '{c}'".format(c=command)
+    logger.info("Running '{c}'".format(c=command))
     p = subprocess.Popen(command, env=os.environ, shell=True,
                          stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
@@ -64,12 +65,34 @@ def parse_options():
     (options, args) = parser.parse_args()
     return options
 
+def set_logger():
+    # create logger
+    logger = logging.getLogger('simple_example')
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    fh = logging.FileHandler("fetch_splunk.log", mode='a')
+    fh.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    fh.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(fh)
+    return logger
+
 def main():
     options = parse_options()
     branch = options.branch
     cl = options.cl
     pkg_dir = options.pkg_dir
     splunk_dir = options.splunk_dir
+
+    global logger
+    logger = set_logger()
 
     # check if pkg_dir and splunk_dir exist. if not, create them
     if not os.path.exists(pkg_dir):
@@ -79,17 +102,20 @@ def main():
         os.makedirs(splunk_dir)
 
     if branch is None:
-        print "Branch must be given, please use '-b' option to specify a branch"
+        logger.error("Branch must be given, please use "
+                     "'-b' option to specify a branch")
         exit(1)
 
     # getting the pkg url
     url_template = ("http://releases.splunk.com/cgi-bin/splunk_build_fetcher.py"
                     "?PLAT_PKG=Linux-x86_64.tgz&DELIVER_AS=url&BRANCH={b}")
     if cl is not None:
-        print "Getting splunk on branch '{b}' @ {cl}".format(b=branch, cl=cl)
+        logger.info("Getting splunk on branch '{b}' @ {cl}".format(b=branch,
+                                                                   cl=cl))
         url_template = url_template + "&P4CHANGE={c}".format(c=cl)
     else:
-        print "Getting the latest splunk pkg on branch '{b}'".format(b=branch)
+        logger.info("Getting the latest splunk pkg on branch '{b}'"
+                    .format(b=branch))
 
     # get url of latest package
     try:
@@ -97,19 +123,19 @@ def main():
         pkg_url = f.readline().strip()
 
         if "Error" in pkg_url:
-            print pkg_url
+            logger.error(pkg_url)
             exit(1)
         elif "" == pkg_url:
-            print ("The pkg url is an empty string, "
+            logger.error("The pkg url is an empty string, "
                    "please check your parameters are correct")
-            print "Brach: {b}, build: {c}".format(b=branch, c=cl)
+            logger.error("Brach: {b}, build: {c}".format(b=branch, c=cl))
             exit(1)
         else:
             # run dluntar
             dluntar(url=pkg_url, tar_dir=pkg_dir, run_dir=splunk_dir)
     except IOError, err:
-        print url_template.format(b=branch)
-        print err
+        logger.error(url_template.format(b=branch))
+        logger.error(err)
         exit(1)
 
 if __name__ == '__main__':
